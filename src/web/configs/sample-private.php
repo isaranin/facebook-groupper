8b93a7c0fcffdd16324dafe6b5d5f022190f4ca0
Ivan Saranin <ivan@saranin.com>
2016-03-11 13:59:57 +0700

    add db data to config

--- src/web/configs/sample-private.php
+++ src/web/configs/sample-private.php
@@ -24,11 +24,21 @@
  */
 
 $res = (object)[
+	//facebook config
 	'facebook' => (object) [
 		'id' => 'app-id',
 		'secret' => 'app-secret',
 		'version' => 'v2.5',
+		//can take it here https://developers.facebook.com/tools/accesstoken/
 		'accessToken' => 'user-access-token'
+	],
+	//database config
+	'db' => (object) [
+		'host' => 'localhost',
+		'base' => 'some-db-name',
+		'user' => 'some-user-name',
+		'password' => 'some-password',
+		'charset' => 'utf8'
 	]
 ];
 
