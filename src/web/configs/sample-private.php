799545260fed36185aa8a3bc93102a8792a693fe
Ivan Saranin <ivan@saranin.com>
2016-03-11 12:30:04 +0700

    add sample private config file

---
+++ src/web/configs/sample-private.php
@@ -0,0 +1,34 @@
+<?
+
+/* 
+ * facebook-groupper
+ * https://github.com/isaranin/facebook-groupper
+ *  
+ * Worker with facebook groups
+ *  
+ * repository		git@github.com:isaranin/facebook-groupper.git
+ *  
+ * author		Ivan Saranin
+ * company		Saranin Co.
+ * url			http://saranin.co
+ * copyright		(c) 2016, Saranin Co.
+ *  
+ *  
+ * created by Ivan Saranin <ivan@saranin.com>, on 11.03.2016, at 12:22:10
+ */
+
+/* 
+ * Module sample
+ * 
+ * Sample private config file
+ */
+
+$res = (object)[
+	'facebook' => (object) [
+		'id' => 'app-id',
+		'secret' => 'app-secret',
+		'version' => 'v2.5'
+	]
+];
+
+return $res;
