5766e5241924a03964844d56afdfb928a4140099
Ivan Saranin <ivan@saranin.com>
2016-03-16 00:05:24 +0700

    change test files

--- src/web/test/api-connect-test.php
+++ src/web/test/api-connect-test.php
@@ -5,35 +5,15 @@
  */
 
 include '../bootstrap.php';
-$fb = new Facebook\Facebook([
-  'app_id' => $_CONFIG->private->facebook->id,
-  'app_secret' => $_CONFIG->private->facebook->secret,
-  'default_graph_version' => $_CONFIG->private->facebook->version,
-  'default_access_token' => $_CONFIG->private->facebook->accessToken, // optional
-]);
 
-// Use one of the helper classes to get a Facebook\Authentication\AccessToken entity.
-//   $helper = $fb->getRedirectLoginHelper();
-//   $helper = $fb->getJavaScriptHelper();
-//   $helper = $fb->getCanvasHelper();
-//   $helper = $fb->getPageTabHelper();
+$fb = new Groupper\FB\Connector(
+	$_CONFIG->private->facebook->id,
+	$_CONFIG->private->facebook->secret,
+	$_CONFIG->private->facebook->version
+);
 
-try {
-  // Get the Facebook\GraphNodes\GraphUser object for the current user.
-  // If you provided a 'default_access_token', the '{access-token}' is optional.
-  $response = $fb->sendRequest('GET','/search', ['q' => 'Phuketbuysellrent', 'type' => 'group']);
-  $response2 = $fb->sendRequest('GET','', ['id' => 'https://www.facebook.com/groups/Phuketbuysellrent/']);
-  //$response->getBody();
+$fb->connect($_CONFIG->private->facebook->accessToken);
 
-} catch(Facebook\Exceptions\FacebookResponseException $e) {
-  // When Graph returns an error
-  echo 'Graph returned an error: ' . $e->getMessage();
-  exit;
-} catch(Facebook\Exceptions\FacebookSDKException $e) {
-  // When validation fails or other local issues
-  echo 'Facebook SDK returned an error: ' . $e->getMessage();
-  exit;
-}
-var_dump($response->getDecodedBody());
-var_dump($response2->getDecodedBody());
-  //var_dump($response);
+$r = $fb->request('GET', '/me');
+var_dump($r);
+var_dump($r->getDecodedBody());
--- src/web/test/db-connect-test.php
+++ src/web/test/db-connect-test.php
@@ -12,11 +12,15 @@ $db = new MysqliDb\MysqliDb(
 		null,
 		$_CONFIG->private->db->charset);
 
-$group = new \Groupper\Model\Group();
-//$a = $group->ArrayBuilder()->orderBy("id", "desc")->get();
-$a = $group->byId(2);
-var_dump($a);
+//$group = new \Groupper\Model\Group();
+////$a = $group->ArrayBuilder()->orderBy("id", "desc")->get();
+//$a = $group->byId(2);
+//var_dump($a);
+//
+//$feeds = new \Groupper\Model\FeedItem();
+//$a = $feeds->byId(['postid'=>1, 'groupid'=>1]);
+//var_dump($a);
 
-$feeds = new \Groupper\Model\FeedItem();
-$a = $feeds->byId(['postid'=>1, 'groupid'=>1]);
-var_dump($a);
+$collection = \Groupper\Model\Collection::join('\Groupper\Model\Group', 'group_id');
+var_dump($collection->get());
+var_dump($collection);
